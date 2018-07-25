918892a7b35d9128fa6e479591c8b3a597c680ec
Rafael Sá <rafael_jdsb@hotmail.com>
2018-07-25 10:20:16 -0300

    curso

---
+++ meat-app-starter/src/app/order/order.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import{RadioOption} from '../shared/radio/radio-option.model'
+import { OrderService } from './order.service';
+import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
+import 'rxjs/add/operator/map'
+
+@Component({
+  selector: 'mt-order',
+  templateUrl: './order.component.html'
+})
+export class OrderComponent implements OnInit {
+
+  delivery: number = 8
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de débito', value: 'DEB'},
+    {label: 'Cartão refeição', value: 'REF'}
+  ]
+
+  constructor(private orderService: OrderService) { }
+
+  ngOnInit() {
+  }
+
+
+  itemsValue(): number{
+    return this.orderService.itemsValue()
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItems()
+  }
+
+  increaseQty(item: CartItem){
+    this.orderService.inscreaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order){
+
+    //map converte um array de cartItems para um de OrderItems
+    order.orderItems = this.cartItems()
+      .map((item:CartItem)=> new OrderItem(item.quantity, item.menuItem.id))
+    
+      this.orderService.checkOrder(order)
+        .subscribe((orderId: string) => {
+          console.log(`Compra concluída: ${orderId}`)
+          this.orderService.clear()
+    } ) 
+    console.log(order)
+  }
+
+}
